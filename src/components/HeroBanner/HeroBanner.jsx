21c205cbfd3c294821984370f4759df6816d06a6
SAMALA SHREE <samala.shree2024@vitstudent.ac.in>
2026-03-25 11:53:06 +0530

    initial changes

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -3,10 +3,10 @@ import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import './HeroBanner.css';
 
 const images = [
-    "https://m.media-amazon.com/images/I/61lwJy4B8PL._SX3000_.jpg", // Kitchen text
-    "https://m.media-amazon.com/images/I/71Ie3JXGfVL._SX3000_.jpg", // Toys
-    "https://m.media-amazon.com/images/I/71U-Q+N7PXL._SX3000_.jpg", // Gaming
-    "https://m.media-amazon.com/images/I/61zAjw4bqPL._SX3000_.jpg"  // Beauty
+    "https://m.media-amazon.com/images/I/61lwJy4B8PL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/71Ie3JXGfVL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/71U-Q+N7PXL._SX3000_.jpg",
+    "https://m.media-amazon.com/images/I/61zAjw4bqPL._SX3000_.jpg"
 ];
 
 function HeroBanner() {
@@ -15,7 +15,7 @@ function HeroBanner() {
     useEffect(() => {
         const timer = setInterval(() => {
             setCurrentIdx((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-        }, 5000); // auto slide every 5s
+        }, 5000);
         return () => clearInterval(timer);
     }, []);
 
@@ -29,7 +29,10 @@ function HeroBanner() {
 
     return (
         <div className="heroBanner">
-            <div className="heroBanner__slider" style={{ transform: `translateX(-${currentIdx * 100}%)` }}>
+            <div 
+                className="heroBanner__slider" 
+                style={{ transform: `translateX(-${currentIdx * 100}%)` }}
+            >
                 {images.map((image, index) => (
                     <img
                         key={index}
@@ -47,7 +50,6 @@ function HeroBanner() {
                 <MdChevronRight />
             </button>
 
-            {/* Fade into rest of page */}
             <div className="heroBanner__fadeBottom" />
         </div>
     );
